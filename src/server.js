7dd243e223559ed5d39a76082b9b3cc1e6789648
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-02-15 16:21:56 -0400

    feat: add aws s3 image upload

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const multer = require("multer")
+const {storage} = require("./configs/multer-config")
+const {uploadFileS3} = require("./configs/s3-config")
+require("dotenv").config()
 
 const app = express()
 app.use(express.json())
+const upload = multer({ storage: storage })
 
 const data = [
   {
@@ -50,6 +55,13 @@ app.post("/book", (req, res) => {
 
   return res.status(201).json({"data": newBook})
 
+})
+
+app.post("/avatar", upload.single('avatar'), (req, res) => {
+  console.log(req.file)
+  uploadFileS3(req.file.filename)
+
+  return res.status(200).json({data: req.file.filename})
 })
 
 app.listen(3333, () => {
